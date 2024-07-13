3fd0e68dfb953ddf62c32e2ad9df57fd30edecb6
adham-omran-ii <adham.a@kapita.iq>
2024-07-13 08:32:09 +0300

    Update `com.iraqidata.tablecloth`

--- src/com/iraqidata/tablecloth.clj
+++ src/com/iraqidata/tablecloth.clj
@@ -1,5 +1,6 @@
 (ns com.iraqidata.tablecloth
-  (:require [tablecloth.api :as tc]))
+  (:require [tablecloth.api :as tc]
+            [scicloj.kindly.v4.kind :as kind]))
 
 ;; Loading data from a CSV file
 (tc/dataset "./resources/data/flights.csv")
@@ -14,14 +15,18 @@
 ;; Column names
 (tc/column-names ds)
 
-;;  flights |>
-;; filter(dest == "IAH") |>
-;; group_by(year, month, day) |>
-;; summarize(
-;;   arr_delay = mean(arr_delay, na.rm = TRUE)
-;; )
+;; This is the R Code
 
-#_(kind/code "")
+^:kindly/hide-code
+(kind/code "flights |>
+  filter(dest == 'IAH') |>
+  group_by(year, month, day) |>
+  summarize(
+    arr_delay = mean(arr_delay, na.rm = TRUE)
+  )
+")
+
+;; This is the Clojure code
 
 (-> ds
     (tc/select-rows (fn [row]
@@ -29,3 +34,15 @@
                          "IAH")))
     (tc/group-by [:year :month :day])
     (tc/mean :arr_delay))
+
+;; ## Re-order things around
+
+;; R Code
+(kind/code "flights |>
+  arrange(desc(dep_delay))")
+
+;; Clojure Code
+
+(-> ds
+    (tc/reorder-columns [:year :month :day :dep_time])
+    (tc/order-by :dep_delay))
