f8dc8ab5ffaaad0488d5bd0398cb385e183499c8
adham-omran-ii <adham.a@kapita.iq>
2024-08-22 11:13:35 +0300

    Apply grading

--- src/com/iraqidata/tasks/day_2.clj
+++ src/com/iraqidata/tasks/day_2.clj
@@ -12,12 +12,27 @@
 (def total-flights (tc/row-count flights-ds))
 (println "The total number of flights is:" total-flights)
 
+;; Correct, 1 full point
+
 ;; 2. How many unique carriers were there in total?
 (def airlines-ds (tc/dataset "./resources/data/airlines.csv"
                              {:key-fn keyword}))
 
-(def unique-carriers (count (distinct (tc/select-columns airlines-ds :carrier))))
+(def unique-carriers (-> airlines-ds
+                         (tc/select-columns :carrier)
+                         distinct
+                         count))
 (println "The total number of carriers is:" unique-carriers)
+
+;; The code does not run. Zero. There was also no need to use `airlines.csv`.
+
+;; The correct code is
+
+(def unique-carriers-works (-> flights-ds
+                               (tc/unique-by :carrier)
+                               (tc/row-count)))
+unique-carriers
+;; => 16
 
 ;; 3. How many unique airports were there in total?
 (def airports-ds (tc/dataset "./resources/data/airports.csv"
@@ -25,6 +40,8 @@
 
 (def unique-airports (count (distinct (tc/select-columns airports-ds :name))))
 (println "The total number of airports is:" unique-airports)
+
+;; Again, the code does not run. Zero. There was no need to use `airports.csv` here.
 
 ;; 4. What is the average arrival delay for each month?
 (def avg-arrival-delay-per-month
@@ -34,10 +51,14 @@
       (tc/select-columns :month :arrival-delay)))
 (println "Average arrival delay for each month:" avg-arrival-delay-per-month)
 
+;; The code runs but there is no result. Zero.
+
 ;; Optional: Use the `airlines` dataset to get the name of the carrier with the
 ;; highest average distance.
 ;; (def avg-distance-per-carrier
 ;;   (->> airlines-ds
-;;        (tc/group-by :carrier)                
-;;        (tc/aggregate {:distance :mean})      
+;;        (tc/group-by :carrier)
+;;        (tc/aggregate {:distance :mean})
 ;;        (tc/select-columns :carrier :distance)))
+
+;; Final score: 1.
